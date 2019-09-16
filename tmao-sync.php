27dd512f9392f0232c451f2f148915f6a083af0e
Dmitry V. Zimin <pfzim@mail.ru>
2019-09-16 14:41:02 +0300

    Init

---
+++ tmao-sync.php
@@ -0,0 +1,53 @@
+<?php
+
+	if(!defined('ROOTDIR'))
+	{
+		define('ROOTDIR', dirname(__FILE__));
+	}
+
+	if(!file_exists(ROOTDIR.DIRECTORY_SEPARATOR.'inc.config.php'))
+	{
+		header('Location: install.php');
+		exit;
+	}
+
+	require_once(ROOTDIR.DIRECTORY_SEPARATOR.'inc.config.php');
+	require_once(ROOTDIR.DIRECTORY_SEPARATOR.'inc.utils.php');
+	require_once(ROOTDIR.DIRECTORY_SEPARATOR.'inc.db.php');
+
+	error_reporting(E_ALL);
+	define('Z_PROTECTED', 'YES');
+
+	$db = new MySQLDB(DB_RW_HOST, NULL, DB_USER, DB_PASSWD, DB_NAME, DB_CPAGE, TRUE);
+
+	header("Content-Type: text/plain; charset=utf-8");
+
+	$params = array(
+		'Database' =>				'**SECRET**',
+		'UID' =>					'**SECRET**',
+		'PWD' =>					'**SECRET**',
+		'ReturnDatesAsStrings' =>	true
+	);
+
+	$conn = sqlsrv_connect("**SECRET**", $params);
+	if($conn === false)
+	{
+		print_r(sqlsrv_errors());
+		exit;
+	}
+
+	$result = sqlsrv_query($conn, "SELECT [COMP_NAME]
+      ,[PTNUPDTIME]
+      ,[SCRIPT_PTN]
+      ,[AS_PSTIME]
+  FROM [**SECRET**].[dbo].[TBL_CLIENT_INFO] WHERE CLIENTTYPE = 0");
+
+
+	while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
+	{
+		echo $row['COMP_NAME'].", ".$row['PTNUPDTIME'].", ".$row['SCRIPT_PTN'].", ".$row['AS_PSTIME']."<br />\r\n";
+		$db->put(rpv("INSERT INTO @computers (`name`, `ao_ptnupdtime`, `ao_script_ptn`, `ao_as_pstime`) VALUES (!, !, #, !) ON DUPLICATE KEY UPDATE `ao_ptnupdtime` = !, `ao_script_ptn` = #, `ao_as_pstime` = !", $row['COMP_NAME'], $row['PTNUPDTIME'], $row['SCRIPT_PTN'], $row['AS_PSTIME'], $row['PTNUPDTIME'], $row['SCRIPT_PTN'], $row['AS_PSTIME']));
+	}
+
+	sqlsrv_free_stmt($result);
+	sqlsrv_close($conn);
---
+++ tmee-sync.php
@@ -0,0 +1,52 @@
+<?php
+
+	if(!defined('ROOTDIR'))
+	{
+		define('ROOTDIR', dirname(__FILE__));
+	}
+
+	if(!file_exists(ROOTDIR.DIRECTORY_SEPARATOR.'inc.config.php'))
+	{
+		header('Location: install.php');
+		exit;
+	}
+
+	require_once(ROOTDIR.DIRECTORY_SEPARATOR.'inc.config.php');
+	require_once(ROOTDIR.DIRECTORY_SEPARATOR.'inc.utils.php');
+	require_once(ROOTDIR.DIRECTORY_SEPARATOR.'inc.db.php');
+
+	error_reporting(E_ALL);
+	define('Z_PROTECTED', 'YES');
+
+	$db = new MySQLDB(DB_RW_HOST, NULL, DB_USER, DB_PASSWD, DB_NAME, DB_CPAGE, TRUE);
+
+	header("Content-Type: text/plain; charset=utf-8");
+
+	$params = array(
+		'Database' =>				'**SECRET**',
+		'UID' =>					'**SECRET**',
+		'PWD' =>					'**SECRET**',
+		'ReturnDatesAsStrings' =>	true
+	);
+
+	$conn = sqlsrv_connect("**SECRET**", $params);
+	if($conn === false)
+	{
+		print_r(sqlsrv_errors());
+		exit;
+	}
+
+	$result = sqlsrv_query($conn, "SELECT [DeviceName]
+      ,[LastSync]
+      ,[EncryptionStatus]
+  FROM [**SECRET**].[dbo].[Device] WHERE IsDeleted = 0");
+
+
+	while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
+	{
+		echo $row['DeviceName'].", ".$row['LastSync'].", ".$row['EncryptionStatus']."<br />\r\n";
+		$db->put(rpv("INSERT INTO @computers (`name`, `ee_lastsync`, `ee_encryptionstatus`) VALUES (!, !, #) ON DUPLICATE KEY UPDATE `ee_lastsync` = !, `ee_encryptionstatus` = #", $row['DeviceName'], $row['LastSync'], $row['EncryptionStatus'], $row['LastSync'], $row['EncryptionStatus']));
+	}
+
+	sqlsrv_free_stmt($result);
+	sqlsrv_close($conn);
