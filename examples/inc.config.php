1e0bc4a350870b84d2b5e8c6b0578d0c99b661d3
Dmitry V. Zimin <pfzim@mail.ru>
2019-10-16 08:16:47 +0300

    Update

---
+++ examples/inc.config.php
@@ -0,0 +1,30 @@
+<?php
+	define('DB_RW_HOST', 'localhost');
+	define('DB_USER', 'cluser');
+	define('DB_PASSWD', 'Pa$$word');
+	define('DB_NAME', 'cdb');
+	define('DB_CPAGE', 'utf8');
+	define('DB_PREFIX', 'c_');
+
+	define('LDAP_HOST', 'dc.contoso.com');
+	define('LDAP_PORT', 389);
+	define('LDAP_USER', 'contoso\\user');
+	define('LDAP_PASSWD', 'Pa$$word');
+	define('LDAP_BASE_DN', 'DC=contoso,DC=com');
+
+	define('MAIL_HOST', 'smtp.contoso.com');
+	define('MAIL_FROM', 'robot@contoso.com');
+	define('MAIL_FROM_NAME', 'Robot');
+	define('MAIL_AUTH', true);
+	define('MAIL_LOGIN', 'robot@contoso.com');
+	define('MAIL_PASSWD', 'Pa$$word');
+	define('MAIL_SECURE', '');
+	define('MAIL_PORT', 25);
+	define('MAIL_TO', 'admin@contoso.com');
+
+	define('HELPDESK_URL', 'http://helpdesk.contoso.com');
+	define('HELPDESK_LOGIN', 'hduser');
+	define('HELPDESK_PASSWD', 'Pa$$word');
+	define('HELPDESK_COOKIE', 'OperuITAuthCookie');
+
+	define('CDB_URL', 'http://web.contoso.com/cdb');
