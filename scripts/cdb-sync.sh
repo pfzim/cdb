d27d6b157297c2c53a8c4b63790f158cadbe6c92
Dmitry V. Zimin <pfzim@mail.ru>
2019-10-23 17:33:42 +0300

    Update

--- scripts/cdb-sync.sh
+++ scripts/cdb-sync.sh
@@ -3,6 +3,6 @@
 php -f /var/www/html/cdb/sync-ad.php
 php -f /var/www/html/cdb/sync-tmao.php
 php -f /var/www/html/cdb/sync-tmee.php
-php -f /var/www/html/cdb/create-tasks-tmao.php
+php -f /var/www/html/cdb/report-tmao-servers.php
 php -f /var/www/html/cdb/check-tasks-status.php
 php -f /var/www/html/cdb/report-tasks-status.php
