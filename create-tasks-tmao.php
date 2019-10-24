58aef70c5cc741e85ee41141b5d172fb2df4c928
Dmitry V. Zimin <pfzim@mail.ru>
2019-10-24 16:56:09 +0300

    Update

--- create-tasks-tmao.php
+++ create-tasks-tmao.php
@@ -26,7 +26,7 @@
 	// Open new tasks
 
 	$i = 0;
-	if($db->select_assoc_ex($result, rpv("SELECT * FROM @computers WHERE (`flags` & (0x01 | 0x04 | 0x08)) = 0 AND `name` regexp '^(([[:digit:]]{4}-[nNwW])|(OFF[Pp][Cc]-))[[:digit:]]+$' AND `ao_script_ptn` < ((SELECT MAX(`ao_script_ptn`) FROM @computers) - 200)")))
+	if($db->select_assoc_ex($result, rpv("SELECT * FROM @computers WHERE (`flags` & (0x01 | 0x04 | 0x08)) = 0 AND `name` regexp '^(([[:digit:]]{4}-[nNwW])|(OFF[Pp][Cc]-))[[:digit:]]+$' AND `ao_script_ptn` < ((SELECT MAX(`ao_script_ptn`) FROM @computers) - 2900)")))
 	{
 		foreach($result as &$row)
 		{
@@ -44,7 +44,7 @@
 					$i++;
 				}
 			}
-			if($i > 5) break;
+			if($i > 0) break;
 		}
 	}
 
@@ -53,7 +53,7 @@
 	// Close auto resolved tasks
 
 	$i = 0;
-	if($db->select_assoc_ex($result, rpv("SELECT * FROM @computers WHERE (`flags` & 0x08) AND `name` regexp '^(([[:digit:]]{4}-[nNwW])|([Pp][Cc]-))[[:digit:]]+$' AND (`ao_script_ptn` >= ((SELECT MAX(`ao_script_ptn`) FROM @computers) - 200) OR (`flags` & (0x01 | 0x04)))")))
+	if($db->select_assoc_ex($result, rpv("SELECT * FROM @computers WHERE (`flags` & 0x08) AND `name` regexp '^(([[:digit:]]{4}-[nNwW])|([Pp][Cc]-))[[:digit:]]+$' AND (`ao_script_ptn` >= ((SELECT MAX(`ao_script_ptn`) FROM @computers) - 2900) OR (`flags` & (0x01 | 0x04)))")))
 	{
 		foreach($result as &$row)
 		{
@@ -63,13 +63,13 @@
 				$xml = @simplexml_load_string($answer);
 				if($xml !== FALSE)
 				{
-					echo $answer."\r\n";
+					//echo $answer."\r\n";
 					echo $row['name'].' '.$row['ao_opernum']."\r\n";
 					$db->put(rpv("UPDATE @computers SET `flags` = (`flags` & ~0x08) WHERE `id` = # LIMIT 1", $row['id']));
 					$i++;
 				}
 			}
-			break;
+			//break;
 		}
 	}
 
