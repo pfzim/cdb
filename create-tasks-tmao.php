e69670b6c496f0997d9843bfc8a9d79e36097648
Dmitry V. Zimin <pfzim@mail.ru>
2019-11-06 09:13:58 +0300

    Update

--- create-tasks-tmao.php
+++ create-tasks-tmao.php
@@ -26,11 +26,23 @@
 	// Open new tasks
 
 	$i = 0;
+
+	if($db->select_ex($result, rpv("SELECT COUNT(*) FROM @computers WHERE (`flags` & (0x01 | 0x04 | 0x20 | 0x08)) = 0x08")))
+	{
+		$i = intval($result[0][0]);
+	}
+	
 	//if($db->select_assoc_ex($result, rpv("SELECT * FROM @computers WHERE (`flags` & (0x01 | 0x04 | 0x08)) = 0 AND `name` regexp '^(([[:digit:]]{4}-[nNwW])|(OFF[Pp][Cc]-))[[:digit:]]+$' AND `ao_script_ptn` < ((SELECT MAX(`ao_script_ptn`) FROM @computers) - 2900)")))
 	if($db->select_assoc_ex($result, rpv("SELECT * FROM @computers WHERE (`flags` & (0x01 | 0x04 | 0x20 | 0x08)) = 0 AND `name` regexp '^(([[:digit:]]{4}-[nNwW])|([Pp][Cc]-))[[:digit:]]+$' AND `ao_script_ptn` = 0")))
 	{
 		foreach($result as &$row)
 		{
+			if($i >= 70)
+			{
+				echo "Limit reached: 70\r\n";
+				break;
+			}
+
 			//$answer = '<?xml version="1.0" encoding="utf-8"? ><root><extAlert><event ref="c7db7df4-e063-11e9-8115-00155d420f11" date="2019-09-26T16:44:46" number="001437825" rule="" person=""/><query ref="" date="" number=""/><comment/></extAlert></root>';
 
 			$answer = @file_get_contents(HELPDESK_URL.'/ExtAlert.aspx/?Source=cdb&Action=new&Type=tmao&Host='.urlencode($row['name']).'&Message='.urlencode("Выявлена проблема с TMAO\nПК: ".$row['name']."\nВерсия антивирусной базы: ".$row['ao_script_ptn']."\nКод работ: AVCTRL\nhttp://wiki.**SECRET**/Отдел%20ИТ%20Инфраструктуры.Restore_AO_agent.ashx"));
@@ -45,7 +57,6 @@
 					$i++;
 				}
 			}
-			//if($i > 20) break;
 		}
 	}
 
