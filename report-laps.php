35874320ab8b2d95a782ea94c9936181b372a521
Dmitry V. Zimin <pfzim@mail.ru>
2019-11-25 16:36:15 +0300

    Add report for LAPS problems

---
+++ report-laps.php
@@ -0,0 +1,160 @@
+<?php
+	// Create report TMAO
+/*
+	SET @current_pattern = (SELECT MAX(ao_script_ptn) FROM c_computers) - 200;
+	SELECT @current_pattern;
+	SELECT * FROM c_computers WHERE ao_script_ptn < @current_pattern AND ao_script_ptn <> 0 OR (name regexp '^[[:digit:]]{4}-[nN][[:digit:]]+' AND ee_encryptionstatus <> 2);
+
+	SELECT name, ao_script_ptn, ee_encryptionstatus FROM c_computers WHERE ao_script_ptn < (SELECT MAX(ao_script_ptn) FROM c_computers) - 200 AND ao_script_ptn <> 0 OR (name regexp '[[:digit:]]{4}-[nN][[:digit:]]+' AND ee_encryptionstatus <> 2)
+
+	TMAO - Workstations:
+	SELECT
+		`name`,
+		`ao_script_ptn`,
+		DATE_FORMAT(`ao_ptnupdtime`, '%d.%m.%Y %H:%i:%s') AS `last_update`,
+		DATE_FORMAT(`ao_as_pstime`, '%d.%m.%Y %H:%i:%s') AS `last_scan`,
+		`flags`
+	FROM c_computers
+	WHERE (`flags` & (0x0001 | 0x0004)) = 0
+		AND `ao_script_ptn` < (SELECT MAX(`ao_script_ptn`) FROM c_computers) - 200
+		AND `name` regexp '^([[:digit:]]{4}-[NnWw][[:digit:]]{4})|([Pp][Cc]-[[:digit:]]{3})$';
+
+	TMME:
+	SELECT * FROM c_computers WHERE name regexp '^[[:digit:]]{4}-[nN][[:digit:]]+' AND (ee_encryptionstatus <> 2 OR ee_lastsync < DATE_SUB(NOW(), INTERVAL 2 WEEK));
+*/
+
+	if(!defined('ROOTDIR'))
+	{
+		define('ROOTDIR', dirname(__FILE__));
+	}
+
+	if(!file_exists(ROOTDIR.DIRECTORY_SEPARATOR.'inc.config.php'))
+	{
+		header('Location: install.php');
+		exit;
+	}
+
+	error_reporting(E_ALL);
+	define('Z_PROTECTED', 'YES');
+
+	require_once(ROOTDIR.DIRECTORY_SEPARATOR.'inc.config.php');
+	require_once(ROOTDIR.DIRECTORY_SEPARATOR.'inc.utils.php');
+	require_once(ROOTDIR.DIRECTORY_SEPARATOR.'inc.db.php');
+
+function php_mailer($to, $name, $subject, $html, $plain)
+{
+	require_once 'libs/PHPMailer/PHPMailerAutoload.php';
+
+	$mail = new PHPMailer;
+
+	$mail->isSMTP();
+	$mail->Host = MAIL_HOST;
+	$mail->SMTPAuth = MAIL_AUTH;
+	if(MAIL_AUTH)
+	{
+		$mail->Username = MAIL_LOGIN;
+		$mail->Password = MAIL_PASSWD;
+	}
+
+	$mail->SMTPSecure = MAIL_SECURE;
+	$mail->Port = MAIL_PORT;
+					$mail->SMTPOptions = array(
+    					'ssl' => array(
+        					'verify_peer' => false,
+        					'verify_peer_name' => false,
+        					'allow_self_signed' => true
+    					)
+					);
+
+	$mail->setFrom(MAIL_FROM, MAIL_FROM_NAME);
+	$mail->addAddress($to, $name);
+	//$mail->addReplyTo('helpdesk@example.com', 'Information');
+
+	$mail->isHTML(true);
+
+	$mail->Subject = $subject;
+	$mail->Body    = $html;
+	$mail->AltBody = $plain;
+	//$mail->ContentType = 'text/html; charset=utf-8';
+	$mail->CharSet = 'UTF-8';
+	//$mail->SMTPDebug = 4;
+
+	return $mail->send();
+}
+
+	$db = new MySQLDB(DB_RW_HOST, NULL, DB_USER, DB_PASSWD, DB_NAME, DB_CPAGE, TRUE);
+
+	header("Content-Type: text/plain; charset=utf-8");
+
+	$html = <<<'EOT'
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<style>
+		body{font-family: Courier New; font-size: 8pt;}
+		h1{font-size: 16px;}
+		h2{font-size: 14px;}
+		h3{font-size: 12px;}
+		table{border: 1px solid black; border-collapse: collapse; font-size: 8pt;}
+		th{border: 1px solid black; background: #dddddd; padding: 5px; color: #000000;}
+		td{border: 1px solid black; padding: 5px; }
+		.pass {background: #7FFF00;}
+		.warn {background: #FFE600;}
+		.error {background: #FF0000; color: #ffffff;}
+		</style>
+	</head>
+	<body>
+	<h1>Список ПК с отсутствующем либо просроченным паролем локального администратора</h1>
+EOT;
+
+	$table = '<table>';
+	$table .= '<tr><th>Name</th><th>HD Task</th></tr>';
+
+	$i = 0;
+	$opened = 0;
+
+	//		AND m.`dn` LIKE '%".LDAP_OU_COMPANY."'
+	if($db->select_assoc_ex($result, rpv("
+		SELECT m.`id`, m.`name`, m.`dn`, m.`laps_exp`, j1.`flags`
+		FROM @computers AS m
+		LEFT JOIN @tasks AS j1 ON j1.pid = m.id AND (j1.flags & (0x0001 | 0x0800)) = 0x0800
+		WHERE
+			(m.`flags` & (0x0001 | 0x0002 | 0x0004)) = 0
+			AND m.`laps_exp` < DATE_SUB(NOW(), INTERVAL 1 MONTH)
+		GROUP BY m.`id`
+		ORDER BY m.`name`
+	")))
+	{
+		foreach($result as &$row)
+		{
+			$table .= '<tr><td>'.$row['name'].'</td><td>';
+			if(intval($row['flags']) & 0x0400)
+			{
+				$table .= '<a href="'.HELPDESK_URL.'/QueryView.aspx?KeyValue='.$row['operid'].'">'.$row['opernum'].'</a>';
+				$opened++;
+			}
+			$table .= '</td></tr>';
+			$i++;
+		}
+	}
+
+	echo 'Count: '.$i."\r\n";
+
+	$table .= '</table>';
+	$html .= '<p>Открытых заявок: '.$opened.', всего проблемных ПК : '.$i.'</p>';
+	$html .= $table;
+
+	$html .= '<br /><small>Для перезапуска отчёта:<br /><br />1. <a href="'.CDB_URL.'/sync-ad.php">Выполнить синхронизацию с AD</a><br />2. <a href="'.CDB_URL.'/report-laps.php">Сформировать отчёт заново</a></small>';
+	$html .= '</body>';
+
+	if($i > 0)
+	{
+		if(php_mailer(MAIL_TO, MAIL_TO, 'Computers with incorrect names', $html, 'You client does not support HTML'))
+		{
+			echo 'Send mail: OK';
+		}
+		else
+		{
+			echo 'Send mail: FAILED';
+		}
+	}
