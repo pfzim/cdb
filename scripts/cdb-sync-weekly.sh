265bdfea023348129d409071f19fe75a64145da1
Dmitry V. Zimin <pfzim@mail.ru>
2019-11-29 15:22:24 +0300

    Update cron scripts

--- scripts/cdb-sync-weekly.sh
+++ scripts/cdb-sync-weekly.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-php -f /var/www/html/cdb/sync-3par.php
-php -f /var/www/html/cdb/report-3par.php
+php -f /var/www/html/cdb/cdb_cli.php sync-3par
+php -f /var/www/html/cdb/cdb_cli.php report-3par
 pwsh -File /var/cdb/scripts/sync-vm.ps1
