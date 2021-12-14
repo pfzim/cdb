b30a91b15ce0d7b07b0d18ac3308dbd1b9e7b7aa
Stavr Ognev <stavr666@gmail.com>
2021-12-14 15:17:08 +0300

    zabbix: uploading fixed

--- sync-zabbix.php
+++ sync-zabbix.php
@@ -99,25 +99,26 @@
 					if(isset($group['groupid'])){
 						$sGroups .= $group['groupid'].';';
 			}}}
+			$host_fixed = (preg_replace('/^(bcc_)/i','',$host['host']);
 			// each IP = unique record in DB
 			foreach($host['interfaces'] as &$sIP) {
 				$bState = ($host['status']==0?'True':'False');
-				echo  $sIP['ip'].'=> hostname:'.$host['host'].' id:'.$host['hostid'].' proxy:'.$host['proxy_hostid'].' status:'.$bState.' groups:'.$sGroups."\r\n";
+				echo  $sIP['ip'].'=> hostname:'.$host_fixed .' id:'.$host['hostid'].' proxy:'.$host['proxy_hostid'].' status:'.$bState.' groups:'.$sGroups."\r\n";
 				$proc_params = array(
 					array(&$sIP['ip'], SQLSRV_PARAM_IN)
-					,array(&$host['host'], SQLSRV_PARAM_IN)
+					,array(&$host_fixed, SQLSRV_PARAM_IN)
 					,array(&$host['hostid'], SQLSRV_PARAM_IN)
 					,array(&$host['proxy_hostid'], SQLSRV_PARAM_IN)
 					,array(&$bState, SQLSRV_PARAM_IN)
 				);
-				$sql = "EXEC [dbo].[spZabbix_update_bcc] @ipstring = '?', @hostname = '?', @hostid = '?', @proxyid = '?', @statzabbix = '?'";
+				$sql = "EXEC [dbo].[spZabbix_update_bcc] @ipstring = ?, @hostname = ?, @hostid = ?, @proxyid = ?, @statzabbix = ?;";
 				$proc_exec = sqlsrv_prepare($conn, $sql, $proc_params);
 				if (!sqlsrv_execute($proc_exec)) {
-					echo "Procedure spZabbix_update_bcc fail!";
+					echo "Procedure spZabbix_update_bcc fail!\r\n";
 					print_r(sqlsrv_errors());
 					//die;
 				}
-				echo "<br />";
+				echo "---------------------------\r\n";
 			}
 		}
 	} else {
