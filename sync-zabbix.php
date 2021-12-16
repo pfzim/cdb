d71cae0d2f6ef63a3c8a1a174331da416c50f485
Stavr Ognev <stavr666@gmail.com>
2021-12-16 17:06:03 +0300

    zabbix: sync working

--- sync-zabbix.php
+++ sync-zabbix.php
@@ -125,154 +125,22 @@
 					print_r(sqlsrv_errors());
 					//die;
 				}
-				echo "---------------------------\r\n";
+				echo "\r\n---------------------------\r\n";
 			}
 		}
 
 		$result = sqlsrv_query($conn, "SELECT [ip],[hostname],[date] FROM [dbo].[fList_Bcc_Zabbix] ('10.92.104.4');");
-		while($row = sqlsrv_fetch_array($invent_result, SQLSRV_FETCH_ASSOC)) {
+		while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
 			var_dump($row);
 			echo "\r\n";
 		}
 		
+		// CLOSE CONNECTION
+		if($result !== FALSE) {
+			sqlsrv_free_stmt($result);
+		}
+		sqlsrv_close($conn);
 	} else {
 		echo "Authentification error.\r\n";
 	}
 
-	// CLOSE CONNECTION
-	if($result !== FALSE) {
-		sqlsrv_free_stmt($result);
-	}
-	sqlsrv_close($conn);
-
-	/** TODO:
-
-	
-
-	$result = sqlsrv_query($conn, "
-		SELECT
-			[DeviceName],
-			[LastSync],
-			[EncryptionStatus]
-		FROM [".TMEE_DB_NAME."].[dbo].[Device]
-		WHERE IsDeleted = 0
-		ORDER BY [LastSync]
-	");
-
-
-
-
-	foreach($scans['hosts'] as &$host)
-	{
-		//echo '  '.$host['host_id'].': '.$host['hostname']."\r\n";
-
-		$row_id = 0;
-		if($db->select_ex($res, rpv("SELECT d.`id` FROM @devices AS d WHERE d.type = 4 AND d.`name` = ! LIMIT 1", $host['hostname'])))
-		{
-			$row_id = $res[0][0];
-		}
-		else
-		{
-			if($db->put(rpv("INSERT INTO @devices (`type`, `pid`, `name`, `flags`) VALUES (4, 0, !, 0)", $host['hostname'])))
-			{
-				$row_id = $db->last_id();
-			}
-		}
-		
-		if($row_id)
-		{
-			$ch = curl_init(NESSUS_URL.'/scans/'.$scan['id'].'/hosts/'.$host['host_id']);
-
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-ApiKeys: accessKey='.NESSUS_ACCESS_KEY.'; secretKey='.NESSUS_SECRET_KEY.';'));
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			if($result !== FALSE)
-			{
-				$host_details = @json_decode($result, true);
-				foreach($host_details['vulnerabilities'] as &$vuln)
-				{
-					if(intval($vuln['severity']) > 0)
-					{
-						//echo '    '.$vuln['severity_index'].' '.$vuln['severity'].' '.' '.$vuln['plugin_id'].' '.$vuln['plugin_name']."\r\n"; flush();
-
-						if(!$db->select_ex($res, rpv("SELECT v.`plugin_id` FROM @vulnerabilities AS v WHERE v.`plugin_id` = # LIMIT 1", $vuln['plugin_id'])))
-						{
-							$db->put(rpv("
-									INSERT INTO @vulnerabilities (`plugin_id`, `plugin_name`, `severity`, `flags`)
-									VALUES ( #, !, #, #)
-								",
-								$vuln['plugin_id'],
-								$vuln['plugin_name'],
-								$vuln['severity'],
-								0x0000
-							));
-						}
-						// update if needed
-						else
-						{
-							$db->put(rpv("
-									UPDATE
-										@vulnerabilities
-									SET
-										`plugin_name` = !,
-										`severity` = #,
-									WHERE
-										`plugin_id` = #
-									LIMIT 1
-								",
-								$vuln['plugin_name'],
-								$vuln['severity'],
-								$vuln['plugin_id']
-							));
-						}
-
-						if(!$db->select_ex($res, rpv("SELECT s.`id`, s.`scan_date` FROM @vuln_scans AS s WHERE s.`pid` = # AND s.`plugin_id` = # LIMIT 1", $row_id, $vuln['plugin_id'])))
-						{
-							$db->put(rpv("
-									INSERT INTO @vuln_scans (`pid`, `plugin_id`, `scan_date`, `folder_id`, `flags`)
-									VALUES (#, #, !, #, #)
-								",
-								$row_id,
-								$vuln['plugin_id'],
-								$scan_date,
-								$scan['folder_id'],
-								0x0000
-							));
-						}
-						else
-						{
-							if(sql_date_cmp($scan_date, $res[0][1]) > 0)
-							{
-								$db->put(rpv("
-										UPDATE
-											@vuln_scans
-										SET
-											`scan_date` = !,
-											`folder_id` = #,
-											`flags` = (`flags` & ~0x0002)     -- reset Fixed flag
-										WHERE
-											`id` = #
-										LIMIT 1
-									",
-									$scan_date,
-									$scan['folder_id'],
-									$res[0][0]
-								));
-							}
-						}
-						
-						$i++;
-					}
-				}
-			}
-			else
-			{
-				echo 'ERROR'."\r\n\r\n"	;
-			}
-		}
-	}
-*/
