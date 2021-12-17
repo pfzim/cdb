ef5206843e536a02e0e11ae02c1e197f86b27b9f
Stavr Ognev <stavr666@gmail.com>
2021-12-17 14:48:39 +0300

    zabbix: test report

--- report-itinvent-bcc.php
+++ report-itinvent-bcc.php
@@ -1,5 +1,5 @@
 <?php
-	// Report Backup communication channel (ДКС - Дополнительные Каналы Связи)
+	// Report Backup communication channel (ДКС - Дублирующие Каналы Связи)
 
 	/**
 		\file
@@ -29,11 +29,21 @@
 		</style>
 	</head>
 	<body>
-	<h1>Отчёт по Дополнительным Каналам Связи</h1>
+	<h1>Отчёт по Дублирующим Каналам Связи (Backup Communication Channels)</h1>
 EOT;
 
 	$table = '<table>';
-	$table .= '<tr><th>Name</th><th>SN</th><th>IP</th><th>Inv.NO</th><th>Updated</th></tr>';
+	$table .= '<tr><th>Name</th><th>SN</th><th>IP</th><th>Inv.NO</th><th>Updated</th><th>Hostname in Zabbix</th></tr>';
+
+	// Connect to CtulhuDB to check values from Zabbix
+	$params = array(
+		'Database' =>				CTULHU_DB_NAME,
+		'UID' =>					CTULHU_DB_USER,
+		'PWD' =>					CTULHU_DB_PASSWD,
+		'ReturnDatesAsStrings' =>	true
+	);
+	$conn_ctulhu = sqlsrv_connect(CTULHU_DB_HOST, $params);
+	if($conn_ctulhu === false) { print_r(sqlsrv_errors()); }
 
 	$i = 0;
 	if($db->select_assoc_ex($result, rpv("
@@ -52,15 +62,21 @@ EOT;
 	AND PORT LIKE 'self'
 	")))
 	{
-
-		foreach($result as &$row)
-		{
+		foreach($result as &$row) {
+			$host_zbx = "";
+			$ret_zbx = sqlsrv_query($conn_ctulhu, "SELECT [ip],[hostname] FROM [dbo].[fList_Bcc_Zabbix] ('".$row['ip']."');");
+			if(!empty($ret_zbx)){
+				$zbx_row = sqlsrv_fetch_array($ret_zbx, SQLSRV_FETCH_ASSOC);
+				$host_zbx = strtoupper($row["hostname"]));
+			}
+	
 			$table .= '<tr>';
 			$table .= '<td>'.$row['name'].'</td>';
 			$table .= '<td><a href="'.CDB_URL.'/cdb.php?action=get-mac-info&id='.$row['id'].'">'.$row['mac'].'</a></td>';
 			$table .= '<td>'.$row['ip'].'</td>';
 			$table .= '<td>'.$row['inv_no'].'</td>';
 			$table .= '<td>'.$row['last_update'].'</td>';
+			$table .= '<td>'.$host_zbx.'</td>';
 			$table .= '</tr>';
 
 			$i++;
@@ -75,7 +91,9 @@ EOT;
 	$html .= '</body>';
 
 	echo 'Total BCC: '.$i."\r\n";
-
+	echo '----------------------------------------'."\r\n";
+	echo $table;
+/*
 	if(php_mailer(array(MAIL_TO_ADMIN, MAIL_TO_NET), CDB_TITLE.': List BCCs', $html, 'You client does not support HTML'))
 	{
 		echo 'Send mail: OK';
