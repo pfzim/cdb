66eff6eeb3073f0a9942e06ec5a09aa949b25013
Dmitriy V. Zimin <Dmitriy.Zimin@bristolcapital.ru>
2022-03-28 15:25:13 +0300

    Update cron scripts

--- scripts/cdb-cron-daily.sh
+++ scripts/cdb-cron-daily.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-pwsh -File /var/cdb/scripts/sync-exch.ps1
-php -f /var/www/html/cdb/cdb.php cron-daily
+pwsh -File /var/cdb/scripts/sync-exch.ps1 >> /var/log/cdb/cron-daily-pwsh.log
+php -f /var/www/html/cdb/cdb.php cron-daily >> /var/log/cdb/cron-daily.log
--- scripts/cdb-cron-weekly.sh
+++ scripts/cdb-cron-weekly.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-pwsh -File /var/cdb/scripts/sync-vm.ps1
-php -f /var/www/html/cdb/cdb.php cron-weekly
+pwsh -File /var/cdb/scripts/sync-vm.ps1 >> /var/log/cdb/cron-weekly-pwsh.log
+php -f /var/www/html/cdb/cdb.php cron-weekly >> /var/log/cdb/cron-weekly.log
