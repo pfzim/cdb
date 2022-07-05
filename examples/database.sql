4e906201c91bf2b0e5ca596798c99b0311f6e610
Dmitriy V. Zimin <Dmitriy.Zimin@bristolcapital.ru>
2022-07-05 15:40:19 +0300

    Updated examples/database.sql

--- examples/database.sql
+++ examples/database.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.17  Distrib 10.4.10-MariaDB, for Linux (x86_64)
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: cdb
 -- ------------------------------------------------------
--- Server version	10.4.10-MariaDB
+-- Server version	10.4.24-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -42,7 +42,24 @@ CREATE TABLE `c_ac_log` (
   `flags` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `i_app_path` (`app_path`(1024)) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2381 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `c_access`
+--
+
+DROP TABLE IF EXISTS `c_access`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c_access` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sid` varchar(256) NOT NULL DEFAULT '',
+  `dn` varchar(1024) NOT NULL DEFAULT '',
+  `oid` int(10) unsigned NOT NULL DEFAULT 0,
+  `allow_bits` binary(32) NOT NULL DEFAULT '\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,10 +80,26 @@ CREATE TABLE `c_computers` (
   `ee_encryptionstatus` int(10) unsigned NOT NULL DEFAULT 0,
   `laps_exp` datetime DEFAULT NULL,
   `sccm_lastsync` datetime DEFAULT NULL,
+  `delay_checks` date DEFAULT '0000-00-00',
   `flags` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `i_name` (`name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=4083300 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `c_config`
+--
+
+DROP TABLE IF EXISTS `c_config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c_config` (
+  `uid` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `value` varchar(8192) NOT NULL DEFAULT '',
+  PRIMARY KEY (`name`,`uid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -85,7 +118,7 @@ CREATE TABLE `c_devices` (
   `flags` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `i_name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=21808 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,7 +136,7 @@ CREATE TABLE `c_files` (
   PRIMARY KEY (`id`),
   KEY `i_filename` (`filename`),
   FULLTEXT KEY `i_path` (`path`)
-) ENGINE=InnoDB AUTO_INCREMENT=830384 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -118,7 +151,27 @@ CREATE TABLE `c_files_inventory` (
   `fid` int(10) unsigned NOT NULL,
   `scan_date` datetime NOT NULL,
   `flags` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`pid`,`fid`) USING BTREE
+  PRIMARY KEY (`pid`,`fid`) USING BTREE,
+  KEY `i_fid` (`fid`),
+  KEY `i_pid` (`pid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `c_logs`
+--
+
+DROP TABLE IF EXISTS `c_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c_logs` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `uid` int(10) unsigned NOT NULL,
+  `operation` varchar(1024) NOT NULL,
+  `params` varchar(4096) NOT NULL,
+  `flags` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -133,13 +186,16 @@ CREATE TABLE `c_mac` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `pid` int(10) unsigned NOT NULL DEFAULT 0,
   `name` varchar(255) NOT NULL DEFAULT '',
-  `inv_no` varchar(16) NOT NULL DEFAULT '',
+  `inv_no` varchar(16) NOT NULL DEFAULT '0',
+  `type_no` int(11) NOT NULL DEFAULT -1,
+  `model_no` int(11) NOT NULL DEFAULT -1,
   `status` int(10) unsigned NOT NULL DEFAULT 0,
   `branch_no` int(11) NOT NULL DEFAULT -1,
   `loc_no` int(10) unsigned NOT NULL DEFAULT 0,
-  `mac` varchar(45) NOT NULL DEFAULT '',
+  `mac` varchar(60) NOT NULL,
   `ip` varchar(45) NOT NULL DEFAULT '',
   `port` varchar(45) NOT NULL DEFAULT '',
+  `vlan` smallint(5) unsigned DEFAULT NULL,
   `first` datetime DEFAULT NULL,
   `date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `comment` varchar(512) NOT NULL DEFAULT '',
@@ -147,8 +203,10 @@ CREATE TABLE `c_mac` (
   PRIMARY KEY (`id`),
   KEY `i_name` (`name`),
   KEY `i_mac` (`mac`),
-  KEY `i_status` (`status`)
-) ENGINE=InnoDB AUTO_INCREMENT=158050 DEFAULT CHARSET=utf8;
+  KEY `i_status` (`status`),
+  KEY `i_loc_no` (`loc_no`),
+  KEY `i_port` (`port`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -163,6 +221,22 @@ CREATE TABLE `c_mac_sn` (
   `sn` varchar(45) NOT NULL,
   `pid` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`mac`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `c_names`
+--
+
+DROP TABLE IF EXISTS `c_names`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c_names` (
+  `type` int(10) unsigned NOT NULL,
+  `pid` int(10) unsigned NOT NULL DEFAULT 0,
+  `id` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`type`,`pid`,`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -185,7 +259,7 @@ CREATE TABLE `c_net_errors` (
   PRIMARY KEY (`id`),
   KEY `i_pid` (`pid`),
   KEY `i_port` (`port`)
-) ENGINE=InnoDB AUTO_INCREMENT=4612 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,7 +278,7 @@ CREATE TABLE `c_persons` (
   `lname` varchar(255) NOT NULL,
   `flags` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14251 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -266,12 +340,13 @@ CREATE TABLE `c_tasks` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `tid` int(10) unsigned NOT NULL DEFAULT 1,
   `pid` int(10) unsigned NOT NULL,
+  `type` int(10) unsigned NOT NULL DEFAULT 0,
   `flags` int(10) unsigned NOT NULL DEFAULT 0,
   `date` datetime NOT NULL,
   `operid` varchar(36) NOT NULL,
-  `opernum` varchar(10) NOT NULL,
+  `opernum` varchar(12) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30073 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -286,10 +361,11 @@ CREATE TABLE `c_users` (
   `login` varchar(255) NOT NULL,
   `passwd` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `mail` varchar(1024) CHARACTER SET latin1 NOT NULL,
-  `sid` varchar(15) DEFAULT NULL,
+  `sid` varchar(16) DEFAULT NULL,
+  `reset_token` varchar(16) DEFAULT NULL,
   `flags` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -326,7 +402,7 @@ CREATE TABLE `c_vuln_scans` (
   PRIMARY KEY (`id`),
   KEY `i_plugin_id` (`plugin_id`),
   KEY `i_pid` (`pid`)
-) ENGINE=InnoDB AUTO_INCREMENT=410654 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -343,7 +419,7 @@ CREATE TABLE `c_vulnerabilities` (
   `flags` int(10) unsigned NOT NULL,
   PRIMARY KEY (`plugin_id`),
   KEY `i_severity` (`severity`)
-) ENGINE=InnoDB AUTO_INCREMENT=151370 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -360,6 +436,22 @@ CREATE TABLE `c_vv_history` (
   `usr_rawrsvd_mb` int(10) unsigned NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `c_zabbix_hosts`
+--
+
+DROP TABLE IF EXISTS `c_zabbix_hosts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c_zabbix_hosts` (
+  `name` varchar(255) NOT NULL,
+  `pid` int(10) unsigned NOT NULL,
+  `host_id` int(10) unsigned NOT NULL DEFAULT 0,
+  `flags` int(10) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -370,4 +462,4 @@ CREATE TABLE `c_vv_history` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-08 17:32:43
+-- Dump completed on 2022-07-05 15:17:24
